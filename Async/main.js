7a9df93ebdef53cafe0d4bd38f864e22dcfed797
Ваня Хортоломей <hortolomei1989@gmail.com>
2021-12-16 23:27:33 +0200

    Update practice

--- Async/main.js
+++ Async/main.js
@@ -1,1 +1,44 @@
-console.log('async');
+//callback function
+
+const btn = document.querySelector('.button');
+
+function getPosition(opts) {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(success => {
+          resolve(success);
+        },
+        error => {
+          reject(error);
+        }, opts)
+  })
+}
+
+//Promises
+function setTimer(duration) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (resolve) {
+        resolve('Done!');
+      } else {
+        reject('Error!')
+      }
+    }, duration);
+  });
+}
+
+
+function trackHandler() {
+  let positionData;
+  getPosition()
+      .then(posData => {
+        positionData = posData;
+        return setTimer(2000);
+      })
+      .then(data => {
+        console.log(data, positionData);
+      })
+}
+
+window.addEventListener('load', () => {
+  btn.addEventListener('click', trackHandler);
+});
